774d8d53f6f7895194ed6e94a1dc6f47a18240cb
Lisa Dünte <lisa.duente@gmail.com>
2022-09-07 11:36:57 +0200

    able to change comment and title in one component

--- js/Comments.js
+++ js/Comments.js
@@ -3,18 +3,10 @@ var popup = document.getElementById("popUp");
 // need to itterate over all buttons
 var btn = document.getElementById("Edit1");
 
-// Get the <span> element that closes the modal
-//var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the popup
 btn.onclick = function() {
   popup.style.display = "block";
 }
-/*
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}*/
 
 // When the user clicks anywhere outside of the popup-content, close it
 window.onclick = function(event) {
@@ -22,3 +14,24 @@ window.onclick = function(event) {
     popup.style.display = "none";
   }
 } 
+
+function getValue(componentID){
+  return document.getElementById(`${componentID}`).value;
+}
+
+//need a good way to put in all the different names of components when they autogenerate
+function setCommentAndTitle(titleID, commentID, newTitleID, newCommentID){
+  let titleContainer = document.getElementById(`${titleID}`);
+  let commentContainer = document.getElementById(`${commentID}`);
+  let newTitle = getValue(newTitleID);
+  let newComment = getValue(newCommentID);
+
+
+  titleContainer.innerHTML = newTitle;
+  commentContainer.innerHTML = newComment;
+  document.getElementById(`${newTitleID}`).value ="";
+  document.getElementById(`${newCommentID}`).value ="";
+  popup.style.display = "none";
+
+
+}
