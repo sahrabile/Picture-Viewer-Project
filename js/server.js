bd6ab5be8305cf41ed16432b99e48e539d5d10ea
Lisa Dünte <lisa.duente@gmail.com>
2022-09-14 10:07:28 +0200

    saving json to file

--- js/server.js
+++ js/server.js
@@ -1,7 +1,11 @@
+//all libraries we need 
 var express = require('express');
 const cors = require('cors');
 var bodyParser = require('body-parser');
 var app = express();
+const fs = require('fs');
+
+//create perfect environment for app
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(cors({
@@ -10,12 +14,16 @@ app.use(cors({
 
 var port = 3000;
 
+//endpoint 
 app.post('/saveJson', function(req, res) {
     console.log('receiving data ...');
     console.log("response status: "+res.statusCode);
     console.log('body is ',req.body);
     res.send(req.body);
     //now update json file!
+    const reqBody = JSON.stringify(req.body);
+    const data = `{"albums":${reqBody}}`;
+    writeToFile(data);
 });
 
 // start the server
@@ -23,14 +31,12 @@ app.listen(port);
 console.log('Server started! At http://localhost:' + port);
 
 
-//helper functions to read and write JSON
-function writeJSON(fname, obj) {
-  const appDataDir = path.join(__dirname, `app_data`);
-  fs.writeFileSync(path.resolve(appDataDir, fname), JSON.stringify(obj));
-}
-
-function readJSON(fname) {
-  const appDataDir = path.join(__dirname, `app_data`);
-  obj = JSON.parse(fs.readFileSync(path.resolve(appDataDir, fname), 'utf8'));
-  return obj;
+//helper functions to write JSON
+function writeToFile(data){
+  fs.writeFile("app-data\\library\\picture-library2.json", data, (err) => {
+    if(err){
+      throw err;
+    }
+    console.log("Data saved");
+  })
 }
